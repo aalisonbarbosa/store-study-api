7cce18d2d9c99b914e83c6e957953813515f0801
Alison Barbosa <alisonnbarbosaaa@gmail.com>
2025-11-14 12:48:41 -0300

    feat: configure cors

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,24 @@
 import Fastify from "fastify";
+import cors from "@fastify/cors";
 import authPlugin from "./plugins/auth.js";
 import authRoutes from "./routes/auth-routes.js";
 import productRoutes from "./routes/product-routes.js";
 import fastifyMultipart from "@fastify/multipart";
 import cartRoutes from "./routes/cart-routes.js";
 import orderRoutes from "./routes/order-routes.js";
+import categoryRoutes from "./routes/category-routes.js";
 
 const fastify = Fastify({
-    logger: true,
+  logger: true,
+});
+
+await fastify.register(cors, {
+  origin: "http://localhost:3000",
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
 });
 
 fastify.register(fastifyMultipart, {
-    attachFieldsToBody: false,
+  attachFieldsToBody: false,
 });
 
 fastify.register(authPlugin);
@@ -20,5 +27,6 @@ fastify.register(authRoutes, { prefix: "/api" });
 fastify.register(productRoutes, { prefix: "/api" });
 fastify.register(cartRoutes, { prefix: "/api" });
 fastify.register(orderRoutes, { prefix: "/api" });
+fastify.register(categoryRoutes, { prefix: "/api" });
 
 fastify.listen({ port: 8080, host: "0.0.0.0" });
